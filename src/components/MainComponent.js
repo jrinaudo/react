29620c4f4b1bfe69f1eae47ad55e1e9c39426196
Juan Manuel Rinaudo <juan.rinaudo@jemstep.com>
2019-06-07 17:06:22 -0300

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,8 +3,12 @@ import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import { DISHES } from '../shared/dishes';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 class Main extends Component {
 
@@ -12,7 +16,9 @@ class Main extends Component {
     super(props);
     this.state = {
         dishes: DISHES,
-        selectedDish: null
+        comments: COMMENTS,
+        promotions: PROMOTIONS,
+        leaders: LEADERS
     };
   }
 
@@ -20,12 +26,16 @@ class Main extends Component {
     this.setState({ selectedDish: dishId});
   }
 
-  render() {
-      const HomePage = () => {
-          return(
-              <Home />
-          );
-      }
+    render() {
+        const HomePage = () => {
+            return(
+                <Home
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
+            );
+        }
 
       return(
           <div>
@@ -34,6 +44,7 @@ class Main extends Component {
                   <Route path='/home' component={HomePage} />
                   <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
                   <Redirect to="/home" />
+                  <Route exact path='/contactus' component={Contact} />} />
               </Switch>
               <Footer />
           </div>
